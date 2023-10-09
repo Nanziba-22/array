30682f6c66cf79614cb87ab792ad406d7820dcbe
Nanziba-22 <webdeveloper.nanziba247@gmail.com>
2023-10-09 22:04:15 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let StudentDetails = [
+{Name: 'Nanziba', Depertment: 'Computer', Roll: '427424', Registation: '150201422', Address: 'Kushtia, Bangladesh', 
+Mobile: '017*****326', Oquality: 'medium', Assaignment: {FirstAssaignment: '70%', LasstAssaignment: '60%'}},
+{Name: 'Shourov', Depertment: 'Electrical', Roll: '4277119', Registation: '150201423', Address: 'Kushtia, Bangladesh', 
+Mobile: '017*****992', Oquality: 'good', Assaignment: {FirstAssaignment: '90%', LasstAssaignment: '70%'}},
+{Name: 'Oindrila', Depertment: 'Civil', Roll: '452178', Registation: '150501444', Address: 'Dhaka, Bangladesh', 
+Mobile: '018*****500',Oquality: 'low', Assaignment: {FirstAssaignment: '75%', LasstAssaignment: '85%'}},
+{Name: 'Topu', Depertment: 'Mechanical', Roll: '427424', Registation: '150201422', Address: 'Foridpur, Bangladesh', 
+Mobile: '018*****110', Oquality: 'medium', Assaignment: {FirstAssaignment: '75%', LasstAssaignment: '85%'}},
+{Name: 'Abdullah', Depertment: 'Computer', Roll: '427424', Registation: '150201422', Address: 'Pabna, Bangladesh',
+ Mobile: '016*****789', Oquality: 'good', Assaignment: {FirstAssaignment: '50%', LasstAssaignment: '75%'}},
+{Name: 'Rabby', Depertment: 'Textile', Roll: '184526', Registation: '150426473', Address: 'Jessor, Bangladesh', 
+Mobile: '017*****002',Oquality: 'low', Assaignment: {FirstAssaignment: '40%', LasstAssaignment: '65%'}},
+
+]
+
+console.log(StudentDetails[4].Roll);
